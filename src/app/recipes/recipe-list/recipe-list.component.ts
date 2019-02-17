7bdb4171ad354bc2619895891bf2a3fbdd2705d6
Tanmay <tanmayawasthi105@gmail.com>
2019-02-17 16:29:25 +0530

    Recipe component with forms

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
+import {Subscription} from "rxjs/internal/Subscription";
 
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html',
     styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
     recipes: Recipe[] = [];
+    recipeChangedSubs: Subscription;
 
     constructor(private recipeService: RecipeService) {
+        this.recipeChangedSubs = this.recipeService.recipeChanged.subscribe(
+            (recipes) => this.recipes = recipes
+        )
     }
 
     ngOnInit() {
         this.recipes = this.recipeService.getRecipe();
+    }
+
+    ngOnDestroy() {
+        this.recipeChangedSubs.unsubscribe();
     }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,12 +2,15 @@ import {Injectable, EventEmitter} from '@angular/core';
 import {Recipe} from "./recipe.model";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs/internal/Subject";
 
 @Injectable({
     providedIn: 'root'
 })
 export class RecipeService {
-    
+
+    recipeChanged = new Subject<Recipe[]>();
+
     private recipes: Recipe[] = [
         new Recipe(
             'A Test Recipe 1 ',
@@ -40,5 +43,20 @@ export class RecipeService {
     }
 
     constructor(private slService: ShoppingListService) {
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipeChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, recipe: Recipe) {
+        this.recipes[index] = recipe;
+        this.recipeChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipeChanged.next(this.recipes.slice());
     }
 }
