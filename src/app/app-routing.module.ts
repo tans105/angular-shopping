0203824155ef30576ff865b0931794182417579d
Tanmay <tanmayawasthi105@gmail.com>
2019-03-31 18:19:20 +0530

    migrated core functionality into a separate module and implemented lazy loading for recipes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {HomeComponent} from "./core/home/home.component";
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    {path: '', component: HomeComponent},
+    {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'}, //lazy loading of recipe module
     {path: 'shopping-list', component: ShoppingListComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,26 @@ import {HttpModule} from "@angular/http";
 import {AppComponent} from './app.component';
 
 import {AppRoutingModule} from "./app-routing.module";
-import {RecipesModule} from "./recipes/recipes.module";
 import {SharedModule} from "./shared/shared.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {AuthModule} from "./auth/auth.module";
 
-import {HeaderComponent} from './header/header.component';
-
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
-import {RecipeService} from "./recipes/recipe.service";
-import {DataStorageService} from "./shared/data-storage.service";
-import {AuthService} from "./auth/auth.service";
-import {AuthGuardService} from "./auth/auth-guard.service";
+import {CoreModule} from "./core/core.module";
 
 
 @NgModule({
     declarations: [
-        AppComponent,
-        HeaderComponent,
+        AppComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         HttpModule,
-        RecipesModule,
         SharedModule,
         ShoppingListModule,
-        AuthModule
+        AuthModule,
+        CoreModule
     ],
-    providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
